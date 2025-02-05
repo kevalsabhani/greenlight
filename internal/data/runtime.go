6a27968fa9ebd3527fef22a29ba7d1bf3910b266
kevalsabhani <kevalsabhani@gmail.com>
2025-02-05 22:15:37 +0530

    add body params validation and json validation

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,39 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int32
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJsonValue := strconv.Quote(jsonValue)
 	return []byte(quotedJsonValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(data []byte) error {
+	text, err := strconv.Unquote(string(data))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(text, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return err
+	}
+
+	*r = Runtime(i)
+	return nil
 }
