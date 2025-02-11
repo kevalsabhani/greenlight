1b71e226f67aa101c2dfee5956ca6c2b78736fb5
kevalsabhani <kevalsabhani@gmail.com>
2025-02-11 22:22:29 +0530

    update response helper function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -27,7 +27,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 }
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
-	js, err := json.MarshalIndent(data, "", "\t")
+	js, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
