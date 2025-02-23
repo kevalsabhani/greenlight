03a75125e1f69f8ae41aadfef91afd5a6ffe2d13
kevalsabhani <kevalsabhani@gmail.com>
2025-02-23 20:58:38 +0530

    add register user route and call handler

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,5 +1,59 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
 
-func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {}
+	"github.com/kevalsabhani/greenlight/internal/data"
+	"github.com/kevalsabhani/greenlight/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &data.User{
+		Name:      input.Name,
+		Email:     input.Email,
+		Activated: false,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
