9bedc67a6cf09071c2fc48e9e491e2519134f43d
kevalsabhani <kevalsabhani@gmail.com>
2025-03-10 21:30:09 +0530

    add enable cors middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -38,6 +39,9 @@ type config struct {
 		password string
 		sender   string
 	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -67,6 +71,10 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "8c6dd85acbd148", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "aa7276776904f0", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <noreply@greenlight.kevalsabhani.net>", "SMTP sender")
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 	flag.Parse()
 
 	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
