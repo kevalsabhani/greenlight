18b55c85b276a4700f687978ff93a01eddf416e4
Keval Sabhani <60646434+kevalsabhani@users.noreply.github.com>
2025-03-12 14:34:42 +0530

    add metrics route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,15 +12,25 @@ func (app *application) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	// health check route
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	
+	// movies routes
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission("movie:write", app.createMovieHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requirePermission("movie:read", app.listMovieHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requirePermission("movie:read", app.showMovieHandler))
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requirePermission("movie:write", app.updateMovieHandler))
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requirePermission("movie:write", app.deleteMovieHandler))
+	
+	// user routes
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
+
+	// auth routes
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	// metrics route
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
